70d300758db61637e6a254f2e192b6984e6793ef
Jack Middlebrook <jack.middlebrook@gmail.com>
2020-01-19 01:53:43 +0100

    Add header to contact form

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -7,6 +7,7 @@ import ContactForm from './ContactForm';
 export const Contact = () => (
   <Wrapper as={Container} id="contact">
     <Details>
+      <h1>Contact Me</h1>
       <ContactForm />
     </Details>
     <Thumbnail>
